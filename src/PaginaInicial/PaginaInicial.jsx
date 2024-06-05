d497bfc8b059014104a66107090a7279412a87db
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-06-05 10:12:28 -0300

    identação

--- src/PaginaInicial/PaginaInicial.jsx
+++ src/PaginaInicial/PaginaInicial.jsx
@@ -1,27 +1,27 @@
 import "./PaginaInicial.css";
 import { useState } from "react";
 
-export default function PaginaInicial(){
+export default function PaginaInicial() {
     let [numeroAleatorio, setNumeroAleatorio] = useState(0);
 
-    function gerarNumero(){
-        const numero = Math.floor(Math.random()*(100));
+    function gerarNumero() {
+        const numero = Math.floor(Math.random() * (100));
         setNumeroAleatorio(numero);
     };
 
-    return(
+    return (
         <div className='conteudo-centralizado'>
-        <h3> Numero aleatório</h3>
-        <h1>{numeroAleatorio}</h1>
-        <div className ="area-botao">
-            <label>
-                Clique no botão abaixo para gerar um número aleatório:
-            </label>
-            <button onClick={gerarNumero}>
-                Gerar numero
-            </button>
+            <h3> Numero aleatório</h3>
+            <h1>{numeroAleatorio}</h1>
+            <div className="area-botao">
+                <label>
+                    Clique no botão abaixo para gerar um número aleatório:
+                </label>
+                <button onClick={gerarNumero}>
+                    Gerar numero
+                </button>
+            </div>
         </div>
-        </div>
-    
-        );
+
+    );
 }
